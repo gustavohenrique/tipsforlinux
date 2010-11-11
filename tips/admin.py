f4a20d788f3aa209fbb2d993298b03d9a6f6e2a4
Gustavo Henrique <gustavo@gustavohenrique.net>
2010-11-11 00:13:36 -0200

    Added notification to author when some tip was rejected by admin

--- tips/admin.py
+++ tips/admin.py
@@ -2,6 +2,7 @@
 from django.contrib.admin import site, ModelAdmin
 from django.contrib.sitemaps import ping_google
 from django.conf import settings
+from django.core.mail import EmailMessage
 
 import twitter
 import tinyurl
@@ -16,8 +17,39 @@ class TipAdmin(ModelAdmin):
     list_per_page = 20
     date_hierarchy = 'pub_date'
     ordering = ('-id', '-pub_date')
-    actions = ['approve', 'post_twitter']
-    
+    actions = ['approve', 'reject', 'post_twitter']
+
+    def _notificate_user_about_rejection_for(self, tip):
+        
+        message_pt_br = u"""\
+        Desculpe.
+        
+        A dica %s foi rejeitada no site tipsforlinux.com.
+        Há 2 possíveis motivos para isso:
+
+        1. A dica talvez tenha sido duplicada ou há outro dica muito parecida.
+        2. A dica é inapropriada. O assunto não tem relação com o tipo de conteúdo do site.
+
+        Agradecemos pela compreensão!
+        """ % tip.title
+
+        message_en_us = u"""\
+        Sorry.
+
+        The tip %s has been rejected on tipsforlinux.com.
+        There are two possible reasons for this:
+
+        1. The tip was maybe duplicated or there is another tip very similar.
+        2. The tip is inappropriate. The subject is not on the contents of the site.
+
+        Thank you for understanding!
+        """ % tip.title
+
+        subject = 'Your tip has been rejected on tipsforlinux.com website'
+        message = "%s\n\n----------------------------\n\n\n%s" % (message_pt_br, message_en_us)
+        mail = EmailMessage(subject, message, settings.DEFAULT_FROM_EMAIL, [tip.author.email, ])
+        mail.send()
+        
     def approve(self, request, queryset):
         queryset.update(approved=True)
         try:
@@ -25,6 +57,16 @@ class TipAdmin(ModelAdmin):
         except Exception:
             pass
     approve.short_description = 'Approve'
+
+    def reject(self, request, queryset):
+        queryset.update(approved=False)
+        first_tip = [ tip for tip in queryset ][0]
+        try:
+            self._notificate_user_about_rejection_for(first_tip)
+            ping_google()
+        except Exception:
+            pass
+    reject.short_description = 'Reject'
     
     def post_twitter(self, request, queryset):
         for tip in queryset:
@@ -46,7 +88,6 @@ class TipAdmin(ModelAdmin):
                 api.PostUpdate(POST_MESSAGE)
             except:
                 pass            
-            
     post_twitter.short_description = 'Post in Twitter'
     
 site.register(Tip, TipAdmin)
